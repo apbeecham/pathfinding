dee62b337cd8b9d95579f27f46fc72544e4f73b7
Adam Beecham <apbeecham91@gmail.com>
2014-02-20 16:23:10 +0000

    Comment updates
    Added Java documentation

--- com/apbeecham/pathfinding/Cell.java
+++ com/apbeecham/pathfinding/Cell.java
@@ -2,32 +2,53 @@ package com.apbeecham.pathfinding;
 
 import java.awt.*;
 
+/************************************************************
+ * Provides a visual representation of a vertex so they can
+ * be drawn on the grid component.
+ * @Author Adam Beecham
+ ************************************************************/
 public class Cell extends Vertex {
-	
+
 	private int width;
 	private int height;
 	private Color color;
-	
+
+	/************************************************************
+	 * Constructs the cell object
+	 * @param position the x and y pixel coordinates of the cell
+	 * @param width the pixel width of the cell
+	 * @param height the pixel height of the cell
+	 ************************************************************/
 	public Cell(Point position, int width, int height){
 		super(position);
 		this.width = width;
-		this.height = height;	
+		this.height = height;
 		this.color = Color.WHITE;
 	}
-	
+
+	/************************************************************
+	 * Set the fill color of the cell.
+	 * @param color the color of the cell
+	 ************************************************************/
 	public void setColor(Color color){
 		this.color = color;
 	}
-	
+
+	/************************************************************
+	 * Get the color of this cell
+	 ************************************************************/
 	public Color getColor(){
 		return this.color;
 	}
-	
+
+	/************************************************************
+	 * Draw the cell
+	 ************************************************************/
 	public void draw(Graphics g){
 		g.setColor(color);
 		g.fillRect(position.x, position.y, width, height);
 		g.setColor(Color.BLACK);
 		g.drawRect(position.x, position.y, width, height);
-		
+
 	}
 }
--- com/apbeecham/pathfinding/Grid.java
+++ com/apbeecham/pathfinding/Grid.java
@@ -4,67 +4,96 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
+/************************************************************
+ * A visual representation of a uniform grid representing the
+ * search space of the pathfinding class.
+ * @author Adam Beecham
+ ************************************************************/
 public class Grid extends JPanel implements MouseListener{
-	
+
 	private int width;
 	private int height;
-	
+
 	private int rowHeight;
 	private int columnWidth;
-	
+
 	private int rows;
 	private int columns;
-	
+
 	private boolean isSearching;
 	private boolean isStartPositionable;
 	private boolean isGoalPositionable;
-	
+
 	private Cell cells[][];
 	private Cell startCell;
 	private Cell goalCell;
-	
+
 	private Pathfinder pathfinder;
-		
+
+	/************************************************************
+	 * Constructs and initialises the Grid object
+	 * @param width the pixel width of the grid
+	 * @param height the pixel height of the grid
+	 * @param rows the number of rows in the grid
+	 * @param columns the number of columns in the grid
+	 ************************************************************/
 	public Grid(int width, int height, int rows, int columns){
-		
+
 		this.width = width;
 		this.height = height;
-		
+
 		this.rows = rows;
 		this.columns = columns;
-		
+
 		rowHeight = height / rows;
 		columnWidth = width / columns;
-		
+
 		isSearching = false;
 		isStartPositionable = true;
 		isGoalPositionable = false;
-		
-		buildGraph();				
+
+		buildGraph();
 		addMouseListener(this);
-		
+
 		this.setPreferredSize(new Dimension(width,height));
 	}
-	
+
+	/************************************************************
+	 * Start the pathfinding algorithm for this grid
+	 * @param step the time between each iteration of the
+	 * algorithm in milliseconds
+	 * @param algorithm the search algorithm to use.
+	 ************************************************************/
 	public void start(int step , int algorithm){
 		isSearching = true;
-		pathfinder.findShortestPath(startCell, goalCell, this, step, algorithm);	
+		pathfinder.findShortestPath(startCell, goalCell, this, step, algorithm);
 	}
-	
+
+	/************************************************************
+	 * Refresh the grid and draw any updates to the screen
+	 ************************************************************/
 	public void update(){
 		this.repaint();
 	}
-	
+
+	/************************************************************
+	 * Reset the grid, interrupting any currently running
+	 * searches.
+	 ************************************************************/
 	public void reset(){
 		isSearching = false;
 		pathfinder.stop();
 		pathfinder = new Pathfinder();
-		buildGraph();		
+		buildGraph();
 		this.repaint();
 	}
-	
 
+	/************************************************************
+	 * Builds the grid graph for the pathfinder to search. Each
+	 * cell in the graph is connected to all of its 8 neighbours
+	 * with and edge, diagonal connections costing slightly more
+	 * than horizontal or vertical edges
+	 ************************************************************/
 	private void buildGraph(){
 		pathfinder = new Pathfinder();
 		cells = new Cell[rows][columns];
@@ -73,7 +102,7 @@ public class Grid extends JPanel implements MouseListener{
 				cells[i][j] = new Cell(new Point(i * columnWidth,j * rowHeight),columnWidth,rowHeight);
 			}
 		}
-		
+
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
 				if(i + 1 < rows)
@@ -92,18 +121,23 @@ public class Grid extends JPanel implements MouseListener{
 					cells[i][j].addEdge(new Edge((int)(rowHeight * 1.4), cells[i + 1][j - 1]));
 				if(i - 1 >= 0 && j + 1 < columns)
 					cells[i][j].addEdge(new Edge((int)(rowHeight * 1.4), cells[i - 1][j + 1]));
-				
+
 			}
 		}
-		
+
 		startCell = cells[0][0];
 		startCell.setColor(Color.MAGENTA);
 		goalCell = cells[rows -1][columns - 1];
 		goalCell.setColor(Color.RED);
 		update();
-		
-	}	
-	
+
+	}
+
+	/************************************************************
+	 * enables the start or goal cell to be repositioned,
+	 * depending on the celltype selected by the user.
+	 * @param cellType the type of cell to be positioned
+	 ************************************************************/
 	public void setPositionable(int cellType){
 		switch(cellType){
 		case 0:
@@ -114,25 +148,32 @@ public class Grid extends JPanel implements MouseListener{
 			isGoalPositionable = true;
 			isStartPositionable = false;
 			break;
-		
+
 		}
 	}
-		
+
+	/************************************************************
+	 * Draws the grid.
+	 ************************************************************/
 	public void paintComponent(Graphics g){
 		g.setColor(Color.BLACK);
 		g.drawRect(0, 0, width, height);
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, width, height);
 		g.setColor(Color.BLACK);
-		
+
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
 				cells[i][j].draw(g);
 			}
 		}
-		
+
 	}
-	
+
+	/************************************************************
+	 * Handle user clicks on the grid, placing the start or goal
+	 * cell at the position the user selected
+	 ************************************************************/
 	public void mouseClicked(MouseEvent e){
 		if(!isSearching){
 			Point mousePos = new Point(e.getX(),e.getY());
@@ -142,8 +183,8 @@ public class Grid extends JPanel implements MouseListener{
 				startCell = cells[(int)(mousePos.x/columnWidth)][(int)(mousePos.y/rowHeight)];
 				startCell.setColor(Color.MAGENTA);
 			}
-			
-			if(isGoalPositionable){			 
+
+			if(isGoalPositionable){
 				if(goalCell != null)
 					goalCell.setColor(Color.WHITE);
 				goalCell = cells[(int)(mousePos.x/columnWidth)][(int)(mousePos.y/rowHeight)];
@@ -152,12 +193,12 @@ public class Grid extends JPanel implements MouseListener{
 			update();
 		}
 	}
-	 
+
 	 public void mouseEntered(MouseEvent e){}
-	 
+
 	 public void mouseExited(MouseEvent e){}
-	 
+
 	 public void mousePressed(MouseEvent e){}
-	 
+
 	 public void mouseReleased(MouseEvent e){}
 }
